5dd36dfaa94bc80e65ba62ec2ebed32bb135db38
thetrufai <chowdhury.f1986@gmail.com>
2022-02-24 14:45:50 -0500

    added new components

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,10 @@
 import './Cart.css';
 
-function Cart(){
-    return <div className= "cart"></div>
+function Cart(props){
+
+    const classes = "card" + props.className;
+
+    return <div className= {classes}> {props.children} </div>
 }
 
 export default Cart;
